ec4a795ce85cfbcd5b4a84202500fbfa9973cead
taohoo3698 <ampla14@gmail.com>
2024-01-29 19:43:19 +0700

    r2.1

--- Manage/lobby.go
+++ Manage/lobby.go
@@ -6,6 +6,9 @@ package Manage
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 type Lobby struct {
@@ -14,6 +17,16 @@ type Lobby struct {
 }
 
 var ActiveLobbies = make(map[string]*Lobby)
+
+func GenerateRandomID() string {
+	rand.Seed(time.Now().UnixNano())
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	b := make([]byte, 8)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
 
 func CreateLobby(host Player, lobbyID string) (*Lobby, error) {
 	newLobby, exists := ActiveLobbies[lobbyID]
@@ -30,7 +43,14 @@ func CreateLobby(host Player, lobbyID string) (*Lobby, error) {
 	return newLobby, nil
 }
 
+var lobbyMutex sync.Mutex
+
+const MaxPlayersInLobby = 4
+
 func JoinLobby(newPlayer Player, lobbyID string) (*Lobby, error) {
+	lobbyMutex.Lock()
+	defer lobbyMutex.Unlock()
+
 	lobby, exists := ActiveLobbies[lobbyID]
 	if !exists {
 		fmt.Println("Lobby", lobbyID, "is not found")
@@ -42,6 +62,11 @@ func JoinLobby(newPlayer Player, lobbyID string) (*Lobby, error) {
 			fmt.Println("Player is already in the lobby:", newPlayer.ID)
 			return nil, errors.New("Player is already in the lobby")
 		}
+	}
+
+	if len(lobby.Players) >= MaxPlayersInLobby {
+		fmt.Println("Lobby is full")
+		return nil, errors.New("Lobby is full")
 	}
 
 	lobby.Players = append(lobby.Players, newPlayer)
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"Unity_Websocket/Client"
+	"Unity_Websocket/Manage"
 	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -12,6 +15,13 @@ func main() {
 
 	http.HandleFunc("/ws/lobby", func(w http.ResponseWriter, r *http.Request) {
 		Client.ServeWsLobby(w, r)
+	})
+
+	r := gin.Default()
+
+	r.GET("/lobbyid", func(c *gin.Context) {
+		lobbyID := Manage.GenerateRandomID()
+		c.JSON(http.StatusOK, gin.H{"randomID": lobbyID})
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
